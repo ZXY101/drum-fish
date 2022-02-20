6bf3801d3c482ef1abfc1ebd1b92d352e8ef976f
ZXY101 <xzxy101x@gmail.com>
2022-02-20 17:12:52 +0200

    fix responsiveness

--- components/ContactUs.tsx
+++ components/ContactUs.tsx
@@ -3,12 +3,12 @@ export default function ContactUs() {
     <div className="text-white px-10 font-mono">
       <h1 className="text-3xl ">Contact us</h1>
       <div className="border-b-2 border-white p-1"></div>
-      <ul className="text-2xl pt-2 pb-20 flex-col gap-1 flex w-2/3">
-        <li className="flex">
+      <ul className="sm:text-2xl text-xl pt-2 pb-20 flex-col gap-2 flex sm:w-2/3">
+        <li className="sm:flex">
           <h2 className="flex-1">Email:</h2>
           <h2 className="flex-1">drumfishband@gmail.com</h2>
         </li>
-        <li className="flex">
+        <li className="sm:flex">
           <h2 className="flex-1">Mobile:</h2>
           <h2 className="flex-1">063-311-1136</h2>
         </li>
